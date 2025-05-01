739c5df02cf302e4b7d053b4c1eff64812fa9dce
Romain <romain.mondija75@gmail.com>
2025-05-01 18:29:40 +0200

    premier projet python

---
+++ home.py
@@ -0,0 +1,13 @@
+from turtle import width
+import streamlit as st
+
+st.markdown('## Montagne')
+st.markdown('### Site sur les montagnes')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("mountain.jpg")
+
+with col2:
+    st.video("https://www.youtube.com/watch?v=TgIokghVsUs")
